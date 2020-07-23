23af2e9f75b4dff201f6df2467ebf52d9db1b71a
egriff89 <eric.griffith42@gmail.com>
2020-07-22 21:20:46 -0500

    Add system monitor functionality

---
+++ app/js/monitor.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const osu = require('node-os-utils');
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+
+// Run every 2 seconds
+setInterval(() => {
+    // CPU Usage
+    cpu.usage().then(info => {
+        document.getElementById('cpu-usage').innerText = `${info}%`;
+    });
+
+    // CPU Free
+    cpu.free().then(info => {
+        document.getElementById('cpu-free').innerText = `${info}%`;
+    });
+
+    // Uptime
+    document.getElementById('sys-uptime').innerText = secondsToDhms(os.uptime());
+}, 2000);
+
+// Set model
+document.getElementById('cpu-model').innerText = cpu.model();
+
+// Computer name
+document.getElementById('comp-name').innerText = os.hostname();
+
+// OS
+document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
+
+// Total memory
+mem.info().then(info => {
+    document.getElementById('mem-total').innerText = info.totalMemMb;
+})
+
+// Show days, hours, mins, sec
+function secondsToDhms(seconds) {
+    seconds = +seconds;
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor((seconds % (3600 * 24)) / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor(seconds % 60);
+    return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
