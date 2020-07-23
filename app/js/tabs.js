656e8f27e3a967755b5a22a0fe1c59df49455fff
egriff89 <eric.griffith42@gmail.com>
2020-07-22 20:43:37 -0500

    Setup interface

--- app/js/tabs.js
+++ app/js/tabs.js
@@ -1,27 +1,27 @@
-const contents = document.querySelectorAll('.content')
-const listItems = document.querySelectorAll('nav ul li')
+const contents = document.querySelectorAll('.content');
+const listItems = document.querySelectorAll('nav ul li');
 
 listItems.forEach((item, index) => {
 	item.addEventListener('click', () => {
-		hideAllContents()
-		hideAllItems()
+		hideAllContents();
+		hideAllItems();
 
 		// activate list item
-		item.classList.add('active')
+		item.classList.add('active');
 
 		// display content
-		contents[index].classList.add('show')
+		contents[index].classList.add('show');
 	})
 })
 
 function hideAllContents() {
 	contents.forEach((content) => {
-		content.classList.remove('show')
+		content.classList.remove('show');
 	})
 }
 
 function hideAllItems() {
 	listItems.forEach((item) => {
-		item.classList.remove('active')
+		item.classList.remove('active');
 	})
 }
